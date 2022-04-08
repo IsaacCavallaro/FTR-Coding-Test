d244f6db15089e50f742d7a65a5f72cb6612708c
isaaccavallaro <isaaccavallaro@gmail.com>
2022-04-09 08:40:36 +1000

    Add Function to display user input on screen

--- main.js
+++ main.js
@@ -6,13 +6,26 @@ let allInput = []
 // Function which runs when user clicks 'Start Here!' button
 function startGame() {
     userInput = prompt("Please input the amount of time in seconds between emitting numbers and their frequency"); // Store the user input in a variable
-    allInput.push(userInput); // Push the usre user input into the allInput array
-    console.log(allInput);
+    allInput.push(userInput); // Push the user input into the allInput array
+    // console.log(allInput[0]);
+    displayUserInput();
 }
+
+// Function to display user input on screen
+function displayUserInput() {
+    let para = document.createElement("p");
+    para.innerText = allInput[0];
+    document.body.appendChild(para);
+}
+
+
 
 // Function which runs after 'x' seconds  based on the user input
 function repeatPromt() {
 
 }
+
+
+
 
 
