3440e355d1f2d14682de51fac46b66f9f986673b
isaaccavallaro <isaaccavallaro@gmail.com>
2022-04-09 20:18:54 +1000

    Create basic quit feature

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
-// Store inital user input for setting the number of milliseconds in array - global scope
-let milliseconds = [];
+// 
+let milliseconds;
 
 // Store conversion of milliseconds to seconds in array - global scope
 let secondsArray = [];
@@ -10,11 +10,32 @@ let allInput = [];
 
 let seconds;
 
+let halt = false;
+
+let resume = false; 
+
+// let quit = false;
+
+let repeatDisplay;
+
+let interval;
+
 // Function runs when user clicks 'Start Here!' button
 function startGame() {
-  let timerInput = prompt("Please input the amount of time in seconds between emitting numbers and their frequency"); // Store the user input in a variable
-  milliseconds.push(timerInput); // Push the user input into the milliseconds array for global scope
+  let timerInput = prompt("Please input the amount of time in seconds between emitting numbers and their frequency"); // Store the user input in timerInput variable
+  milliseconds = timerInput
   displayTimer(); // call displayTimer function
+}
+
+
+// Function runs when user clicks 'halt' button
+function haltGame() {
+  halt = true;
+}
+
+// Function runs when user clicks 'resume' button
+function resumeGame() {
+  resume = true;
 }
 
 // Function to display user input for timer on screen
@@ -26,23 +47,66 @@ function displayTimer() {
 }
 
 function conversion() {
-  convertToSeconds = milliseconds[0] * 1000 // Convert the user input from milliseconds to seconds
-  secondsArray.push(convertToSeconds); // Push the covertToSeconds value to seconds array for global scope
-  seconds = secondsArray[0]
-  // console.log(`this is ${seconds}`)
-  firstNumPrompt()
+  convertToSeconds = milliseconds * 1000 // Convert the user input from milliseconds to seconds
+  seconds = convertToSeconds
+  console.log(`this is ${seconds}`)
+  frequencyDisplay()
 }
 
-function firstNumPrompt() {
-  let numPrompt = prompt("Please enter the first number");
-  allInput.push(numPrompt);
-  setInterval(displayNumPrompt, seconds);
+function frequencyDisplay() {
+  const number = document.getElementById("number");
+  let interval = setInterval(function() {number.innerHTML += `${milliseconds}`}, seconds);
+  
+  // select button with id 'quit and perform function when clicked
+  quit = document.getElementById('quit').onclick = function() { 
+    alert("Thank you for playing the game"); // Thank you message when 'quit' button is clicked
+    clearInterval(interval); // Stop setInterval function when 'quit' button is clicked
+
+    // select paragraph with id 'timer' and remove all text when 'quit' button is clicked
+    let para = document.getElementById("timer");
+    para.innerText = " ";
+    document.body.appendChild(para);
+
+    let numPara = document.getElementById("number");
+    numPara.innerText = " ";
+    document.body.appendChild(numPara);
+    }
 }
 
-
-// function displayNumPrompt() {
-//   document.getElementById("number").innerHTML += "Hello";
+// function quitGame() {
+//   quit = document.getElementById('quit').onclick = function() {
+//   alert("button was clicked");
+//   clearInterval(interval);
+//   };
 // }
+
+  // // let interval = setInterval(repeatNumDisplay, seconds); // call setInterval and use callback displayNumPrompt to be called to the value of seconds
+  // let numPrompt = prompt("Please enter the first number");
+  // allInput.push(numPrompt); // Store the user input number in allInput array
+  
+ 
+  
+
+
+function repeatNumDisplay() {
+
+}
+
+
+
+
+ 
+function next() {
+
+}
+
+// function quitGame() {
+//   console.log("quit");
+//   clearInterval(repeatDisplay);
+// }  
+
+
+
 
 
 
